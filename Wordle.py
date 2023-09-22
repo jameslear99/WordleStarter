a59d56d1513f95ab428913bc4127d69105d3189b
bruyndecker <bruyn.decker@byu.edu>
2023-09-22 16:20:33 -0600

    code

--- Wordle.py
+++ Wordle.py
@@ -9,50 +9,64 @@ import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
-    # secretWord = random.choice(FIVE_LETTER_WORDS)
-    # gw = WordleGWindow()
-    
-
-    def enter_action(s):
-
-         # gw.show_message("You have to implement this method.")
-        currentRow= gw.get_current_row()
-
-        if spellCheck(currentRow) == True:
-            gw.set_current_row(currentRow + 1)
-
-   
-
+    secretWord = random.choice(FIVE_LETTER_WORDS)
     gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-
-
-#Spell check function
-    def spellCheck(currentRow):
+    
+    def enter_action(s):
+        currentRow = gw.get_current_row()
         
+        if spellCheck(currentRow):
+            user_word = getUserWord(currentRow)
+            feedback = getFeedback(user_word, secretWord)
+            
+            gw.set_current_row(currentRow + 1)
+            
+            for col, status in enumerate(feedback):
+                if status == 'C':  # Correct letter and position
+                    gw.set_square_color(currentRow, col, CORRECT_COLOR)
+                elif status == 'P':  # Correct letter, wrong position
+                    gw.set_square_color(currentRow, col, PRESENT_COLOR)
+                else:  # Incorrect letter
+                    gw.set_square_color(currentRow, col, MISSING_COLOR)
+                    
+            
+            if user_word == secretWord:
+                gw.show_message("You win!")
+                return
+            
+            # Is the game over?
+            if currentRow == N_ROWS - 1:
+                gw.show_message("Game Over!")
+                return
+
+    def getUserWord(currentRow):
         word = ""
-        #Grabs all the letters from each column and makes a word
-        for col in range(0, N_COLS):
+        for col in range(N_COLS):
             word += gw.get_square_letter(currentRow, col)
-        #Converts the word to lowercase to match the dictionary
-        word = word.lower()        
-        #Checks if the entered word is found inside the dictionary, then either moves to a new line, or displays error
+        return word.lower()
         
+    def spellCheck(currentRow):
+        word = getUserWord(currentRow)
         if word in FIVE_LETTER_WORDS:
             return True
-            
         else:
             gw.show_message("You fool! This is no word!")
-  
+            return False
+            
+    def getFeedback(user_word, secret_word):
+        feedback = ['M'] * N_COLS
+        for i in range(N_COLS):
+            if user_word[i] == secret_word[i]:
+                feedback[i] = 'C'
+        for i in range(N_COLS):
+            if feedback[i] == 'M' and user_word[i] in secret_word:
+                feedback[i] = 'P'
+        return feedback
 
-        
-        
-
-
-
-# Startup code
+    gw.add_enter_listener(enter_action)
 
 if __name__ == "__main__":
     wordle()
